0d9f6d44eb80902ab380e04f9bd98a380a216d1a
ybalcin <ybalcin1@gmail.com>
2023-07-11 02:34:20 +0300

    fixes

--- RetailStore.Domain/Models/Invoice.cs
+++
@@ -1,9 +0,0 @@
-namespace RetailStore.Domain.Models;
-
-public class Invoice
-{
-    public Bill Bill { get; set; }
-    
-    public float Gross { get; set; }
-    public float Net { get; set; }
-}
--- RetailStore.Domain/Models/Product.cs
+++ RetailStore.Domain/Models/Product.cs
@@ -4,18 +4,4 @@ public class Product
 {
     public string Category { get; set; }
     public float Price { get; set; }
-
-    // private float _discountedPrice;
-    //
-    // public float DiscountedPrice
-    // {
-    //     get
-    //     {
-    //         if (!(_discountedPrice <= 0)) return _discountedPrice;
-    //         
-    //         _discountedPrice = Price;
-    //         return _discountedPrice;
-    //     }
-    //     set => _discountedPrice = value;
-    // }
 }
